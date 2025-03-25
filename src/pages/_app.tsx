5cf652284d0f520000b1571ba2c2b9a7a90f392a
Moldir Andirbekova <moldirandirbekova@Moldirs-MacBook-Pro.local>
2025-03-26 01:57:55 +0500

    render sidebar and header based on the current path. we dont need them in main and login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,26 @@
+import { useRouter } from "next/router";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const showSidebarAndHeader = ["/games", "/feedback", "/guidelines", "/profile", "/reports"];
+  
+  const isVisible = showSidebarAndHeader.includes(router.pathname);
+
   return (
     <div className="flex flex-col min-h-screen bg-[#FFF5E1]">
-      <Header />
-      <div className="flex flex-1">
-        <Sidebar />
-        <Component {...pageProps} />
+      {isVisible && <Header />}
+      {isVisible && (
+        <div className="flex flex-1">
+          <Sidebar />
+          <Component {...pageProps} />
         </div>
+      )}
+      {!isVisible && <Component {...pageProps} />}
     </div>
-  )
+  );
 }

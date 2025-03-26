9e449ba1ed9b4903f197b891c9c52c123d7f6313
Dilnaz <210103077@stu.sdu.edu.kz>
2025-03-26 17:43:40 +0500

    api end points,added guidelines page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,20 @@
-'use client'
+"use client";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import "@/styles/globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen bg-[#FFF5E1]">
         <Header />
         <div className="flex flex-1">
           <Sidebar />
-          <main className="flex-1">{children}</main>  
+          <main className="flex-1">{children}</main>
         </div>
       </body>
     </html>

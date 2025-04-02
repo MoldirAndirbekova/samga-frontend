3e167843f09c49b27a7aa1f14fa391a5ee0f34e3
Moldir Andirbekova <moldirandirbekova@Moldirs-MacBook-Pro.local>
2025-04-02 17:21:12 +0500

    connect profile age with back api

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,31 +1,83 @@
-'use client'
-import { FaCalendarAlt } from 'react-icons/fa';
-import { BiPencil  } from 'react-icons/bi';
+"use client"
+
+import { useState, useEffect } from "react";
+import { BiPencil } from "react-icons/bi";
+import api from "@/features/page"; 
+
+interface User {
+  id: string;
+  full_name: string;
+  email: string;
+  children?: Child[]; 
+}
+
+interface Child {
+  id: string;
+  full_name: string;
+  email: string;
+}
 
 export default function Profile() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await api.get("apis/users/me");
+
+        if (response.data) {
+          setUser(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (!user) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className="flex">
-      <main className="flex-1 p-10  text-[#694800] flex ">
-        <div className="flex flex-col items-center w-1/3  ">
-        <h2 className="mr-13 text-3xl font-bold mb-6 text-start">Profile</h2>
-          <img src="/icons/user-avatar.png " alt="User Avatar" className="w-40 h-40 rounded-xl  bg-[#F9DB63]" />
-          <p className="mt-3 text-xl font-bold">USER</p>
+    <div className="flex flex-col gap-10 p-10 text-[#694800]">
+      <div className="flex items-center gap-10">
+        <div className="flex flex-col items-center w-1/3">
+          <h2 className="text-3xl font-bold mb-6">Account</h2>
+          <img src="/icons/user-avatar.png" alt="User Avatar" className="w-40 h-40 rounded-xl bg-[#F9DB63]" />
+          <p className="mt-3 text-xl font-bold">Personal Account</p>
         </div>
-        <div className="mr-22 flex-1 p-18 relative">
+        <div className="flex-1 p-10 relative">
           <button className="absolute top-3 right-2 text-2xl text-gray-800 hover:text-gray-600">
             <BiPencil />
           </button>
-          <div className="space-y-4 w-160 ">
-            <div className=" bg-[#F9DB63] px-7 py-2 rounded-3xl text-lg font-semibold">Name: User</div>
-            <div className=" bg-[#F9DB63] px-7 py-2 rounded-3xl text-lg font-semibold">Email: user.user@gmail.com</div>
-            <div className=" bg-[#F9DB63] px-7 py-2 rounded-3xl text-lg font-semibold flex items-center gap-2">
-              <span>Date of Birth: 01.01.2015</span>
-              <FaCalendarAlt />
-            </div>
-            <div className=" bg-[#F9DB63] px-7 py-2 rounded-3xl text-lg font-semibold">Phone Number: +7 (707) 777 77 77</div>
+          <div className="space-y-4 w-160">
+            <div className="bg-[#F9DB63] px-7 py-2 rounded-3xl text-lg font-semibold">Name: {user.full_name}</div>
+            <div className="bg-[#F9DB63] px-7 py-2 rounded-3xl text-lg font-semibold">Email: {user.email}</div>
           </div>
         </div>
-      </main>
+      </div>
+
+      <div className="flex items-center gap-10">
+          <div className="flex flex-col items-center w-1/3">
+            <img src="/icons/kid-avatar.png" alt="Child Avatar" className="w-40 h-40 rounded-xl bg-[#F9DB63]" />
+            <p className="mt-3 text-xl font-bold">Player</p>
+          </div>
+          <div className="flex-1 p-10 relative">
+            <button className="absolute top-3 right-2 text-2xl text-gray-800 hover:text-gray-600">
+              <BiPencil />
+            </button>
+            <div className="space-y-4 w-160">
+              {/* {user.children?.map((child) => ( */}
+              {/* key={child.id} */}
+                <div className="space-y-4">
+                  <div className="bg-[#F9DB63] px-7 py-2 rounded-3xl text-lg font-semibold">Name: child name</div>
+                </div>
+              {/* ))} */}
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
